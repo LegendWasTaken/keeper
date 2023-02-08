0e5c856e8b8bbf4f41885e25df7b262262be946d
LegendWasTaken <caiodcw@gmail.com>
2023-02-08 21:15:38 +0800

    cli: update cmake structure of project

---
+++ source/cli.cpp
@@ -0,0 +1,23 @@
+#include "cli.h"
+
+#include <CLI/App.hpp>
+#include <CLI/Formatter.hpp>
+#include <CLI/Config.hpp>
+
+keep::cli::options keep::cli::parse(int argc, char **argv) {
+  auto app = CLI::App("Keeper");
+
+  auto args = options();
+  app.add_flag("--init", args.init, "Should keep initialize base files to the path? (Requires path)");
+  app.add_flag("--write_service", args.write_service, "Write a service file for keeper? (Requires path)");
+
+  app.add_option("--path", args.path, "Path for keeper to use");
+
+  try {
+    app.parse(argc, argv);
+  } catch (CLI::ParseError &e) {
+    // Todo: Log error
+  }
+
+  return args;
+}
--- source/cli.h
+++ source/cli.h
@@ -1,9 +1,7 @@
 #ifndef KEEPER_CLI_H
 #define KEEPER_CLI_H
 
-#include <CLI/App.hpp>
-#include <CLI/Formatter.hpp>
-#include <CLI/Config.hpp>
+#include <string>
 
 namespace keep::cli {
   struct options {
@@ -12,23 +10,14 @@ namespace keep::cli {
 
     std::string path = "";
   };
-  [[nodiscard]] options parse(int argc, char** argv) {
-    auto app = CLI::App("Keeper");
 
-    auto args = options();
-    app.add_flag("--init", args.init, "Should keep initialize base files to the path? (Requires path)");
-    app.add_flag("--write_service", args.write_service, "Write a service file for keeper? (Requires path)");
-
-    app.add_option("--path", args.path, "Path for keeper to use");
-
-    try {
-      app.parse(argc, argv);
-    } catch (CLI::ParseError &e) {
-      // Todo: Log error
-    }
-
-    return args;
-  }
+  /**
+   * Parses arguments from the command line into options struct
+   * @param argc Argument count
+   * @param argv Arguments
+   * @return Options struct with populated values (or defaults)
+   */
+  [[nodiscard]] options parse(int argc, char** argv);
 }
 
 #endif //KEEPER_CLI_H
