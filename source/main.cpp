970c0bea6fd7f1281b6fee891a2736933476887c
LegendWasTaken <caiodcw@gmail.com>
2023-02-08 21:45:57 +0800

    cli: use throwing parse in main

--- source/main.cpp
+++ source/main.cpp
@@ -3,14 +3,7 @@
 #include "cli.h"
 
 int main(int argc, char** argv) {
-  const auto options = [argc, argv](){
-    const auto tried_options = keep::cli::try_parse(argc, argv);
-    if (tried_options.has_value()) {
-      return tried_options.value();
-    } else {
-      throw std::runtime_error("Failed to parse values");
-    }
-  }();
+  const auto options = keep::cli::parse(argc, argv);
 
   return 0;
 }
